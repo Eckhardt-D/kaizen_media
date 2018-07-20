854b4f78a04ba0136677c01722c64d0ff44a84f0
ecki-d <support@kaizenmedia.co.za>
2018-07-20 09:43:49 +0200

    :sparkles: Fix bottom info on packages page

--- src/views/Packages.vue
+++ src/views/Packages.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="about">
       <b-container class="py-5">
-        <b-row class="mt-5">
+        <b-row id="cardHolder" class="mt-5">
           <b-col class="my-2" v-for="(item, i) in packages" :key ="i" lg="4" md="6" sm="12">
             <b-card class="cardAdj">
                 <img class="card-img-top" :src="item.image" alt="">
                 <h5 class="pt-3">{{item.name}}</h5>
                 {{item.desc}}
               <br>
-                <b-btn class="mt-3">Enquire</b-btn>
+                <b-btn class="cardBtn mt-3">Enquire</b-btn>
             </b-card>
           </b-col>
         </b-row>
       </b-container>
       <div class="bottomInfo">
-        <b-row class="pt-3">
+        <b-row class="pt-3 m-0">
           <b-col class="mx-auto pb-5 pl-5" lg="4">
              <b-media class="text-white" tag="li">
               <img src="../assets/eckhardt.jpg" class="li-img" slot="aside"  height="64px" alt="placeholder" />
@@ -61,11 +61,20 @@ export default {
 
 <style lang="scss">
 .about {
+  position: relative;
   min-height: 100vh;
   background-image: url("https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
   background-size: cover;
 
+  #cardHolder {
+    padding-bottom: 40vh;
+  }
+
   .bottomInfo {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    left: 0;
     min-height: 14em;
     background: rgba(128, 0, 128, 0.8);
     .li-img {
@@ -79,7 +88,13 @@ export default {
     }
   }
   .cardAdj {
-      min-height: 361px;
+      min-height: 468.78px;
+      position: relative;
+      .cardBtn {
+        position: absolute;
+        bottom: 10px;
+        left: 10px;
+      }
   }
 }
 </style>
